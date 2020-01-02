6b368853f7ace8489a67184646677b02436f8c43
foo6 <foo6@cornell.edu>
2020-01-02 01:44:12 -0500

    resume website data

---
+++ build/precache-manifest.15529f12e10d5d56a3e3b4ac98526f6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6e900154755242b2c4d",
+    "url": "/static/js/main.8037693d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9f7bc571661205fe9d1",
+    "url": "/static/js/2.ce8a9bb8.chunk.js"
+  },
+  {
+    "revision": "fe1402d8f50393d3b8ffdee796de5a2c",
+    "url": "/index.html"
+  }
+];
